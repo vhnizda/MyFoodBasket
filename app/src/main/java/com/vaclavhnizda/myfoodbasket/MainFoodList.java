07cd98d3f34399aa6cc329514ebb5b54628c4076
vaclav <vaclav.cz@gmail.com>
2017-02-23 10:08:08 +0100

    Initial commit

---
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/MainFoodList.java
@@ -0,0 +1,13 @@
+package com.vaclavhnizda.myfoodbasket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainFoodList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_food_list);
+    }
+}
