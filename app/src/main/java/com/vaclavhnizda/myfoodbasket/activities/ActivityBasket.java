83e5547fde11636d0213fd2f1b263fa9784368a7
vaclav <vaclav.cz@gmail.com>
2017-02-23 10:39:34 +0100

    Initial layout started, will use it to build the framework internals. Later return to polishing the look.

--- app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/ActivityBasket.java
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/ActivityBasket.java
@@ -1,13 +1,14 @@
-package com.vaclavhnizda.myfoodbasket;
+package com.vaclavhnizda.myfoodbasket.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.vaclavhnizda.myfoodbasket.R;
 
-public class MainFoodList extends AppCompatActivity {
+public class ActivityBasket extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_food_list);
+        setContentView(R.layout.activity_basket);
     }
 }
---
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/MainActivityFoodList.java
@@ -0,0 +1,14 @@
+package com.vaclavhnizda.myfoodbasket.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.vaclavhnizda.myfoodbasket.R;
+
+public class MainActivityFoodList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_food_list);
+    }
+}
