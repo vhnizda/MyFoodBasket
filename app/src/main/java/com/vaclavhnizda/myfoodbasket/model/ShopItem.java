991f01919c09d4fe52fc698c5fa4cdff3c997713
vaclav <vaclav.cz@gmail.com>
2017-02-24 10:57:02 +0100

    added shopping item class

---
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/model/ShopItem.java
@@ -0,0 +1,16 @@
+package com.vaclavhnizda.myfoodbasket.model;
+
+/**
+ * Created by vaclav on 2/24/17.
+ */
+
+public class ShopItem {
+    int id;
+    String iconRef;
+    String unitOfMesurement;
+    double priceUSDreference;
+    int stockCount; // use later
+
+    public ShopItem(){}
+
+}
