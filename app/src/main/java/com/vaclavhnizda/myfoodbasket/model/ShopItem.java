596556e81fa9965ca54401abf70828fbd39a9db5
vaclav <vaclav.cz@gmail.com>
2017-02-24 11:14:00 +0100

    inserted SurgarORM library to database management.

--- app/src/main/java/com/vaclavhnizda/myfoodbasket/model/ShopItem.java
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/model/ShopItem.java
@@ -1,10 +1,12 @@
 package com.vaclavhnizda.myfoodbasket.model;
 
+import com.orm.SugarRecord;
+
 /**
  * Created by vaclav on 2/24/17.
  */
 
-public class ShopItem {
+public class ShopItem extends SugarRecord{
     int id;
     String iconRef;
     String unitOfMesurement;
@@ -12,5 +14,13 @@ public class ShopItem {
     int stockCount; // use later
 
     public ShopItem(){}
+
+    public ShopItem(int id, String iconRef, String unitOfMesurement, double priceUSDreference, int stockCount){
+        this.id = id;
+        this.iconRef = iconRef;
+        this.unitOfMesurement = unitOfMesurement;
+        this.priceUSDreference = priceUSDreference;
+        this.stockCount = stockCount;
+    }
 
 }
