eb6b82f49198377136db1af7f3948b240787f26b
vaclav <vaclav.cz@gmail.com>
2017-02-28 11:30:22 +0100

    Used butter knife to link main buttons.

--- app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/ActivityBasket.java
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/ActivityBasket.java
@@ -4,11 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.vaclavhnizda.myfoodbasket.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ActivityBasket extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.main_return_button_in_basket)
+    public void returnToShoping(){
+        this.finish();
+    }
+
+    @OnClick(R.id.currency_button_in_basket)
+    public void currencyChange(){
+        //TODO allow selection of other currencies
     }
 }
--- app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/MainActivityFoodList.java
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/activities/MainActivityFoodList.java
@@ -1,19 +1,43 @@
 package com.vaclavhnizda.myfoodbasket.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.vaclavhnizda.myfoodbasket.R;
 import com.vaclavhnizda.myfoodbasket.controller.ControlManager;
 
-public class MainActivityFoodList extends AppCompatActivity {
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-    private ControlManager myMan;
+public class MainActivityFoodList extends AppCompatActivity {
+    //Manager for logic controls
+    private ControlManager myControlMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_food_list);
+        ButterKnife.bind(this);
 
-        myMan = new ControlManager();
+        myControlMan = new ControlManager();
+
+        //TODO Load data 1. currency type 2. produce we can sell
+        //TODO Generate buttons and link Controller to clicks & add to view
+    }
+
+
+    @OnClick(R.id.basket_button_main)
+    public void launchBasket(){
+        //This is where the basket is launched
+        Intent intent = new Intent(this, ActivityBasket.class);
+        startActivity(intent);
+    }
+
+
+    @OnClick(R.id.currency_button_main)
+    public void currencyChange(){
+        //TODO allow selection of other currencies
     }
 }
