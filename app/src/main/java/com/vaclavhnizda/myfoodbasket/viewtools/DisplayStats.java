ded5aa543354cb65c46086f6b0f20782392c5efa
vaclav <vaclav.cz@gmail.com>
2017-03-16 15:39:53 +0100

    refinement of display stats code.
    Adding workspace.xml file to repository

--- app/src/main/java/com/vaclavhnizda/myfoodbasket/viewtools/DisplayStats.java
+++ app/src/main/java/com/vaclavhnizda/myfoodbasket/viewtools/DisplayStats.java
@@ -1,7 +1,10 @@
 package com.vaclavhnizda.myfoodbasket.viewtools;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
 /**
  * Created by vaclav on 2/24/17.
@@ -9,15 +12,21 @@ import android.util.DisplayMetrics;
 
 public class DisplayStats {
 
-    public static int getWidth(Activity activity){
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+    public static int getWidth(Context context){
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        //int px = (int) TypedValue.applyDimension(TypeValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
         return displayMetrics.widthPixels;
     }
 
-    public static int getHeight(Activity activity) {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+    public static int getHeight(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         return displayMetrics.heightPixels;
+    }
+
+    public static int widthDPtoPixels(Context context,int dpUnits){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float value = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpUnits, displayMetrics);
+        return (int)value;
     }
 }
